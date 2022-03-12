813a68d9512603453f4ca62e49dccf1c757ff24e
decodinatorX <decodinator@gmail.com>
2022-03-12 05:57:45 -0500

    Potestium Addition
    Haven't gotten the custom model for helmet yet but will get to it later.

---
+++ src/main/java/com/decodinator/liroth/core/armor/PotestiumHelmItem.java
@@ -0,0 +1,48 @@
+/*package com.decodinator.liroth.core.armor;
+
+import net.minecraft.item.ArmorItem;
+
+public class PotestiumHelmItem extends ArmorItem implements IHasModel 
+{
+	public PotestiumHelmItem(String name, CreativeTabs tab, ArmorMaterial materialIn, EntityEquipmentSlot equipmentSlotIn)
+	{
+		super(materialIn, 1, equipmentSlotIn);
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(tab);
+		setMaxStackSize(1);
+		
+		ItemInit.ITEMS.add(this);
+	}
+	
+	@Override
+	public void registerModels() 
+	{
+		Main.proxy.registerModel(this, 0);
+	}
+
+	@Override
+	public ModelBiped getArmorModel(EntityLivingBase entityLiving, ItemStack itemStack, EntityEquipmentSlot armorSlot, ModelBiped _default) 
+	{
+		if(itemStack != ItemStack.EMPTY)
+		{
+			if(itemStack.getItem() instanceof ItemArmor)
+			{
+				ModelCustomArmour model = new ModelCustomArmour();
+				
+				model.bipedHead.showModel = armorSlot == EntityEquipmentSlot.HEAD;
+				
+				model.isChild = _default.isChild;
+				model.isRiding = _default.isRiding;
+				model.isSneak = _default.isSneak;
+				model.rightArmPose = _default.rightArmPose;
+				model.leftArmPose = _default.leftArmPose;
+				
+				return model;
+			}
+		}
+		
+		return null;
+	}
+	
+}*/
